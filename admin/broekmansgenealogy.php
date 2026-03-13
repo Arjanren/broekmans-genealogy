e3708ca7720daf45f46439f6d22465b8128f60b5
Arjanren <arjan@broekmans.eu>
2026-03-13 14:41:31 +0100

    Delete admin directory

--- admin/broekmansgenealogy.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-use Joomla\CMS\Factory;
-
-$booted = Factory::getApplication()
-    ->bootComponent('com_broekmansgenealogy');
-
-$booted->getDispatcher()->dispatch();
